aff9ae78e84606bac352d013141797132a686480
voltdogmusic <30582821+voltdogmusic@users.noreply.github.com>
2019-08-26 16:59:01 -0400

    added cowsay

--- index.php
+++ index.php
@@ -13,6 +13,13 @@ $app->get('/', function () use ($app) {
 });
 
 
+
+$app->get('/cowsay', function() use($app) {
+    $app['monolog']->addDebug('cowsay');
+    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+
 $app->get('/readAll', function () use ($app) {
 
     //return 'Hello';
