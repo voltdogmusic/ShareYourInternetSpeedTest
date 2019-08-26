9e0090b3f4925aa6930a0dd024019f7d374ea8c1
voltdogmusic <30582821+voltdogmusic@users.noreply.github.com>
2019-08-26 16:16:06 -0400

    added readAll route within index.php

--- index.php
+++ index.php
@@ -5,8 +5,16 @@ require('./vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+
+// http://localhost/BradRestApi/index.php will redirect the vanllia path to views/index.html
+// https://hidden-brushlands-71469.herokuapp.com also uses this to redirect to index.html
 $app->get('/', function () use ($app) {
     return $app->sendFile(__DIR__.'/views/index.html');
+});
+
+
+$app->get('/readAll', function () use ($app) {
+    return $app->sendFile(__DIR__.'/api/post/read.php');
 });
 
 $app->run();
