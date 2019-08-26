475fce8d4d8d17211d7719027d0a757acf868db8
voltdogmusic <30582821+voltdogmusic@users.noreply.github.com>
2019-08-26 19:39:23 -0400

    changed git ignore to add crazy files
    changed git ignore to add crazy files

--- index.php
+++ index.php
@@ -5,17 +5,18 @@ require('./vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-
-// http://localhost/BradRestApi/index.php will redirect the vanilla path to views/index.html
-// https://hidden-brushlands-71469.herokuapp.com also uses this to redirect to index.html
-$app->get('/', function () use ($app) {
-    return $app->sendFile(__DIR__.'/views/index.html');
-});
-
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => 'php://stderr',
 ));
+
+
+// http://localhost/BradRestApi/index.php will redirect the vanilla path to views/index.html
+ https://hidden-brushlands-71469.herokuapp.com also uses this to redirect to index.html
+$app->get('/', function () use ($app) {
+    return $app->sendFile(__DIR__.'/views/index.html');
+});
+
 
 $app->get('/cowsay', function() use($app) {
     $app['monolog']->addDebug('cowsay');
@@ -23,11 +24,10 @@ $app->get('/cowsay', function() use($app) {
 });
 
 
-$app->get('/readAll', function () use ($app) {
-
-    //return 'Hello';
-
-    return $app->sendFile(__DIR__.'/api/post/read.php');
+$app->get('/myview', function () use ($app) {
+    //$app['monolog']->addDebug('myview');
+    return $app->sendFile(__DIR__.'/views/myview.html');
 });
+
 
 $app->run();
