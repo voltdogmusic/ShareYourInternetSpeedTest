0b0472b0758161a0c7f7a4b34aa60aece6be6eb4
voltdogmusic <30582821+voltdogmusic@users.noreply.github.com>
2019-09-15 16:03:11 -0400

    changed db code in generate table

--- config/Database.php
+++ config/Database.php
@@ -2,13 +2,6 @@
 
 class Database
 {
-
-     //private $host = 'us-cdbr-iron-east-02.cleardb.net';
-     //private $db_name = 'heroku_74da0c35df50742';
-     //private $username = 'b80d61794837eb';
-     //private $password = '9af4c84a';
-
-
     private $host;
     private $db_name;
     private $username;
@@ -39,37 +32,6 @@ class Database
         }
         return $this->conn;
     }
-
-
-
-//    private $conn;
-//
-//
-//    // My code for clearDB database, need to set up if statement here for local development
-//    public function connect()
-//    {
-//
-//        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//
-//        $server = $url["host"];
-//        $username = $url["user"];
-//        $password = $url["pass"];
-//        $db = substr($url["path"], 1);
-//
-//        //$conn = new mysqli($server, $username, $password, $db);
-//
-//        $this->conn = null;
-//        try {
-//            $this->conn = new PDO('mysql:host=' . $server . ';dbname=' . $db, $username, $password);
-//            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-//        } catch (PDOException $e) {
-//            echo 'Connection Error: ' . $e->getMessage();
-//        }
-//        return $this->conn;
-//    }
-
-
-
 }
 
 
